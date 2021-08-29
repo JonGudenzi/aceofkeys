5e644235f60a404de89a942ec3f6037bb2075dd1
JonGudenzi <jongudenzi@gmail.com>
2021-08-29 15:18:27 -0400

    basic layout of homepage and form submit section

---
+++ app.js
@@ -0,0 +1,20 @@
+//This is a vue app object//
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA: 'Learn vue',
+            courseGoalB: 'Master vue',
+            vueLink: 'https://vuejs.org/',
+            email: 'jongudenzi@gmail.com'
+        };
+    },
+    methods: {
+submitEmail(){
+
+},
+
+        
+    }
+});
+
+app.mount('#user-goal');
